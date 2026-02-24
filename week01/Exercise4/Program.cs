dcd94b37ca32bef2ea3f825ba4757adaa9aeef7f
Hgregori <Hgregorio@byupathway.edu>
2026-02-24 18:42:31 -0300

    4 to 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int numb = -1;
+         
+         do
+        {
+            Console.Write("Write a number(0 to stop): ");
+            string num = Console.ReadLine();
+            numb = int.Parse(num);
+
+            numbers.Add(numb);
+        } while (numb != 0);
+
+        int total = 0;
+        int max = 0;
+
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        float average = total/numbers.Count;
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program.");
+        }
+        
+        static void PromptUserName(string name)
+        {
+            Console.WriteLine($"Your name is {name}.");
+        }
+    
+        static void PromptUserNumber(int num)
+        {
+            Console.WriteLine($"Your favorite number is {num}.");
+        }
+
+        static int SquareNumber(int num)
+        {
+            int squared = num * num;
+            return squared;
+        }
+
+        static void DisplayResult(string name, int num)
+        {
+            Console.WriteLine($"Name: {name} | Squared num: {num}");
+        }
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your name: ");
+        string favNum = Console.ReadLine();
+        int num = int.Parse(favNum);
+
+        DisplayWelcome();
+        PromptUserName(name);
+        PromptUserNumber(num);
+        int squared = SquareNumber(num);
+        DisplayResult(name, squared);
+
     }
 }
