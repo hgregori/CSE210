0d867d09a05bab794710f7f168286b4a46795e53
Hgregori <Hgregorio@byupathway.edu>
2026-02-25 18:31:03 -0300

    Learning Activity W2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+}
+
+public class Job
+{
+    public string _company;
+    public  string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void JobInformation()
+    {
+        string job = $"{_jobTitle} ({_company}) {_startYear}-{_endYear}";
+        Console.WriteLine(job);
+
+        myResume1.Display();
+    }
+}
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+      public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
+    }
+
 }
