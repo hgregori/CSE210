bb5f40832b688c0ceb63c9fc8c24530f431d6400
Hgregori <Hgregorio@byupathway.edu>
2026-03-03 23:01:35 -0300

    encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fra1 = new Fraction();
+        Console.WriteLine(fra1.GetFractionString());
+        Console.WriteLine(fra1.GetDecimalValue());
+
+        Fraction fra2 = new Fraction(5);
+        Console.WriteLine(fra2.GetFractionString());
+        Console.WriteLine(fra2.GetDecimalValue());
+
+        Fraction fra3 = new Fraction(3, 4);
+        Console.WriteLine(fra3.GetFractionString());
+        Console.WriteLine(fra3.GetDecimalValue());
+
+        Fraction fra4 = new Fraction(1, 3);
+        Console.WriteLine(fra4.GetFractionString());
+        Console.WriteLine(fra4.GetDecimalValue());
     }
 }
